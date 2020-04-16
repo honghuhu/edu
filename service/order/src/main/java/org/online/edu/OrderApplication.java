2de3657dc69600d746dabbc3ce1ac98794c42f58
) <yxuiang.wang@gmail.com>
2020-04-16 22:02:55 +0800

    A 订单模块

--- service/order/src/main/java/org/online/edu/OrderApplication.java
+++ service/order/src/main/java/org/online/edu/OrderApplication.java
@@ -1,5 +1,6 @@
 package org.online.edu;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
+@MapperScan("org.online.edu.mapper")
 @ComponentScan(basePackages = {"org.online"})
 public class OrderApplication {
 
